478516fe6bd55a0dbbee6efa3d1b6c84ed9bbd33
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2016-11-21 13:03:14 +0100

    Alembic no longer messing up runtime logging

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,6 @@
 from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-from logging.config import fileConfig
 
 from delivery.models.db_models import SQLAlchemyBase
 
@@ -9,9 +8,6 @@ from delivery.models.db_models import SQLAlchemyBase
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
